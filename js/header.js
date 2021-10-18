8b9e2303888dd3e3103ff325fd59bab36f365031
Eunsung Moon <eunsung9647@gmail.com>
2021-10-18 20:53:26 +0900

    페이지 분리하고 싶어서 일단 파일부터 만듬

--- js/header.js
+++ js/header.js
@@ -4,10 +4,17 @@ let createDom = () => {
   let $headerWrap = sel.el('.headerWrap');
   let headerDom=`
     <nav>
-      <div class = "navbar">dfdf
+      <div class = "navbar">
         <ul>
+          <li class = "headLogo"><a href=""></a></li>
           <li>
-          
+          <button class = "headBtn"><a href=""></a><a href=""></a>영화</button>
+          </li>
+          <li>
+            <button class = "headBtn"><a href=""></a>TV 프로그램</button>
+          </li>
+          <li>
+            <button class = "headBtn"><a href=""></a>책</button>
           </li>
         </ul>
       </div>
